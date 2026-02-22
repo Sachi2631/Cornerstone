4cf9b58049437e8e9c1b4ce982e6ce49fb1123c8
Sachi Tsuno <2631Sachi@gmail.com>
2026-02-21 17:57:03 -0800

    db edits

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -11,11 +11,13 @@ if (!mongoURI) {
   throw new Error('MONGO_URI is not defined. Check your .env file.');
 }
 
-// Function to connect to MongoDB
 export const connectDB = async (): Promise<void> => {
   try {
     await mongoose.connect(mongoURI);
+
     console.log('Connected to MongoDB successfully!');
+    console.log('Connected to DB:', mongoose.connection.name);
+
   } catch (error) {
     console.error('MongoDB connection error:', error);
     throw error;
